5bba466b06826fa7a246be5b05ac390281c94295
Isaiah Ernest Ovie <officialisaiahovie@gmail.com>
2023-05-13 16:26:50 +0100

    Modifications: Modify Hero section and product cards

--- components/Home/Products/ProductCard.jsx
+++ components/Home/Products/ProductCard.jsx
@@ -6,32 +6,32 @@ import Link from 'next/link';
 
 const ProductCard = ({ product }) => {
   return (
-    <Link href={`/products/${product?._id}`}>
-      <div className='mx-auto relative w-full max-w-sm overflow-hidden cursor-pointer duration-500 card p-2'>
+    <div className='mx-auto relative w-full max-w-sm overflow-hidden cursor-pointer duration-500 card p-2'>
+      <Link href={`/products/${product?._id}`}>
         <Image
-          className='w-full h-60 object-cover rounded-lg bg-center'
+          className='w-full h-72 object-cover rounded-lg bg-center'
           src={product?.imageUrl}
           alt='Card image'
           width={600}
           height={600}
         />
-        <div className='px-6 py-4'>
-          <p className='text-dark font-semibold mb-2'>{product?.name}</p>
+      </Link>
+      <div className='px-6 py-4'>
+        <p className='text-dark font-semibold mb-2'>{product?.name}</p>
 
-          <p className='font-extrabold text-2xl mb-2 text-dark'>
-            {formatAmountToNaira(product?.price)}
-          </p>
-        </div>
-        <div className='px-2 mt-2 flex gap-8 items-center justify-between'>
-          <button className='w-full px-8 py-4 bg-accent rounded-md font-semibold text-white'>
-            Add to cart
-          </button>
-          <button className='p-2 border-[1px] border-dark rounded-full'>
-            <BsHeart className='cursor-pointer text-red-500' />
-          </button>
-        </div>
+        <p className='font-extrabold text-2xl mb-2 text-dark'>
+          {formatAmountToNaira(product?.price)}
+        </p>
       </div>
-    </Link>
+      <div className='px-2 mt-2 flex gap-8 items-center justify-between'>
+        <button className='w-full px-8 py-4 bg-accent rounded-md font-semibold text-white'>
+          Add to cart
+        </button>
+        <button className='p-2 border-[1px] border-dark rounded-full'>
+          <BsHeart className='cursor-pointer text-red-500' />
+        </button>
+      </div>
+    </div>
   );
 };
 
